a34d2e21d9ef6f577c66391392783fbef34f9f97
Gabriel <gabrielmagalhaes3344@gmail.com>
2024-04-27 13:49:26 -0300

    Commit para salvar os arquivos para realizar pull

--- src/Chess/ChessPosition.java
+++ src/Chess/ChessPosition.java
@@ -1,2 +1,14 @@
-package Chess;public class ChessPosition {
+package Chess;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Invalid chess position. Valid values are from a1 to h8");
+        }
+        this.column = column;
+        this.row = row;
+    }
 }
